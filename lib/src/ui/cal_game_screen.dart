dde509ffa5f3375da3b4d5736d95b8128a8929e2
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-11-01 10:02:07 +0300

    Calculate sayfasına appbar eklendi

--- lib/src/ui/cal_game_screen.dart
+++ lib/src/ui/cal_game_screen.dart
@@ -17,10 +17,71 @@ class _CalculateGameState extends State<CalculateGame> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 228, 182, 182),
-      body: Column(
+      appBar: AppBar(
+        toolbarHeight: 60,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))
+        ),
+        backgroundColor: Color.fromARGB(255, 59, 50, 50),
+        actions: [
+          Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              InkWell(
+                onTap: (() {}),
+                child: Container(
+                  margin: EdgeInsets.only(right: 15),
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 71, 38, 35),
+                      border: Border.all(color: Colors.white, width: 1.5),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: Ikonate(
+                      Ikonate.pause,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+        leadingWidth: 80,
+        title: Row(
+          children: [
+            Spacer(),
+            Ikonate(
+              Ikonate.cup,
+              color: Colors.yellow,
+              height: 35,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              "0",
+              style: GoogleFonts.inter(fontSize: 30, color: Colors.white),
+            ),
+            Spacer(),
+          ],
+        ),
+        leading: Row(
+          children: [
+            SizedBox(
+              width: 15,
+            ),
+            FirstScreenNaviPopWidget()
+          ],
+        ),
+      ),
+      /* body: Column(
         children: [
           SizedBox(
-            height: 25,
+            height: 200,
           ),
           Container(
             height: 100,
@@ -74,46 +135,8 @@ class _CalculateGameState extends State<CalculateGame> {
               ),
             ),
           ),
-          /* ElevatedButton.icon(
-            onPressed: () {},
-            icon: Ikonate(
-              Ikonate.moon,
-              color: Colors.white,
-              height: 30,
-            ),
-            label: Text(
-              "",
-              style: GoogleFonts.inter(
-                fontSize: 17,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                // backgroundColor: Color.fromARGB(255, 233, 89, 89),
-                foregroundColor: Color.fromARGB(255, 211, 197, 155),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                side: BorderSide(
-                    width: 2, color: Color.fromARGB(255, 207, 152, 152))),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Ikonate(
-              Ikonate.moon,
-              color: Colors.white,
-              height: 30,
-            ),
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.black,foregroundColor: Color.fromARGB(255, 211, 197, 155),
-            shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                side: BorderSide(
-                    width: 2, color: Color.fromARGB(255, 207, 152, 152))
-            ),
-          ), */
         ],
-      ),
+      ), */
     );
   }
 }
