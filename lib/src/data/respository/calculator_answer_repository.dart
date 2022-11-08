e31d6adf0df77641c9638146a6e62c71d5d74469
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-11-08 14:57:31 +0300

    Devam edilecek provider dosyaları eksik.

---
+++ lib/src/data/respository/calculator_answer_repository.dart
@@ -0,0 +1,14 @@
+import 'package:mind_game/src/data/models/correct_answer.dart';
+
+class CorrectAnswerRespository{
+  static List<int> listHasCode = <int>[];
+  static getCorrectAnswerDataList(int level){
+    if(level == 1){
+      listHasCode.clear();
+    }
+  }
+
+  List<CorrectAnswer> list = <CorrectAnswer>[];
+
+  while(list.length < 5)
+}
--- lib/src/ui/app/game_provider.dart
+++ lib/src/ui/app/game_provider.dart
@@ -2,9 +2,29 @@ import 'package:flutter/animation.dart';
 import 'package:mind_game/src/core/app_constant.dart';
 import 'package:mind_game/src/ui/app/time_provider.dart';
 
-class GameProvider extends TimeProvider {
+class GameProvider<T> extends TimeProvider {
   final GameCategoryType gameCategoryType;
+  late List list;
+  late int index;
+  late double currentScore;
+  late double oldScore;
+  late T currentState;
+  late String result;
+
 
   GameProvider({required TickerProvider vsync, required this.gameCategoryType})
       : super(vsync: vsync, totalTime: KeyUtil.getTimeUtil(gameCategoryType));
+
+      void startGame(async){
+        result = "";
+      
+      }
+
+      List<T> getList(int level){
+        switch (gameCategoryType){
+          case GameCategoryType.CALCULATOR:
+          
+          break;
+        }
+      }
 }
